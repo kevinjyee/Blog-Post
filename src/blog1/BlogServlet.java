75d6a59a5085f6f554bcf73b3faefecbbef1be76
kevinjyee <kevjyee@gmail.com>
2016-09-16 17:19:09 -0500

    added HTML

--- src/blog1/BlogServlet.java
+++ src/blog1/BlogServlet.java
@@ -2,11 +2,39 @@ package blog1;
 
 import java.io.IOException;
 import javax.servlet.http.*;
+import com.google.appengine.api.users.User;
+
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+
+import com.googlecode.objectify.ObjectifyService;
+import static com.googlecode.objectify.ObjectifyService.ofy;
+
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @SuppressWarnings("serial")
 public class BlogServlet extends HttpServlet {
-	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
-	}
+	static {
+        ObjectifyService.register(BlogPost.class);
+    }
+	
+	public void doPost(HttpServletRequest req, HttpServletResponse resp)
+                throws IOException {
+		
+		UserService userService = UserServiceFactory.getUserService();
+        User user = userService.getCurrentUser();
+        
+        String title = req.getParameter("title");
+        String content = req.getParameter("content");
+        BlogPost post = new BlogPost(user, title, content);
+ 
+        ofy().save().entity(post).now();   // synchronous
+ 
+        resp.sendRedirect("/blog.jsp");
+    }
 }
